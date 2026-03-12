f2109f3d2ba719cba6927827a3b99290e9894433
Vatamaniuk <oleksandr_vatamaniuk@knu.ua>
2026-03-12 13:59:56 +0200

    car class

---
+++ lab04_Builder/task_3_1/Car.java
@@ -0,0 +1,29 @@
+public class Car {
+    public enum CarColor { White, Black, Red, Grey, Blue }
+    public enum Type { Sedan, Hatchback, SUV }
+
+    private final Type type;
+    private final CarColor carColor;
+    private final Engine engine;
+    private final Wheel wheel;
+    private final Transmission transmission;
+
+    public Car(Type type, CarColor carColor, Engine engine,
+               Wheel wheel, Transmission transmission) {
+        this.type = type;
+        this.carColor = carColor;
+        this.engine = engine;
+        this.wheel = wheel;
+        this.transmission = transmission;
+    }
+
+    @Override
+    public String toString() {
+        return "Car:\n"
+                + "  type         = " + type + "\n"
+                + "  color        = " + carColor + "\n"
+                + "  engine       = " + engine + "\n"
+                + "  wheel        = " + wheel + "\n"
+                + "  transmission = " + transmission + "\n";
+    }
+}

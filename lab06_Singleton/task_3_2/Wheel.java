47f5bc45e734e89424c38703d1c506c2401bac9f
Vatamaniuk <oleksandr_vatamaniuk@knu.ua>
2026-03-23 17:03:48 +0200

    add Car component classes

---
+++ lab06_Singleton/task_3_2/Wheel.java
@@ -0,0 +1,16 @@
+public class Wheel {
+    public enum Material { Steel, Alloy, Forged }
+
+    final private Material material;
+    final int diameter;
+
+    public Wheel(Material material, int diameter) {
+        this.material = material;
+        this.diameter = diameter;
+    }
+
+    @Override
+    public String toString() {
+        return "Wheel{material=" + material + ", diameter=" + diameter + "}";
+    }
+}
